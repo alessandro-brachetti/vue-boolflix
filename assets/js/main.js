69011e474b9a559d98b528c1b4e005d7767bd7a6
Alessandro Brachetti <alebrack13@gmail.com>
2021-04-13 15:24:25 +0200

    Milestone 2 e 3 Complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,18 +3,47 @@ Vue.config.devtools = true;
 var app = new Vue({
   el: '#root',
   data: {
-    dataAPI: 'https://api.themoviedb.org/3/search/movie?api_key=29e84b564b7c90c52ba93dc6436ea2c6&query=',
+    uri: 'https://api.themoviedb.org/3',
+    apiKey:'29e84b564b7c90c52ba93dc6436ea2c6',
+    lang: 'it',
     queryInput: '',
-    moviesSearched:[],
+    movies: [],
   },
   methods: {
-    searchMovie: function () {
-      axios.get(this.dataAPI + this.queryInput)
+    search: function() {
+      axios.get(`${this.uri}/search/movie?api_key=${this.apiKey}&language=${this.lang}&page=1&include_adult=false&query=${this.queryInput}`)
         .then((response) => {
-          console.log(response);
-          this.moviesSearched = response.data.results;
-          console.log(this.moviesSearched);
-        });
+          this.movies = [...this.movies, ...response.data.results];
+        })
+      axios.get(`${this.uri}/search/tv?api_key=${this.apiKey}&language=${this.lang}&page=1&include_adult=false&query=${this.queryInput}`)
+        .then((response) => {
+          this.movies = [...this.movies, ...response.data.results];
+          this.queryInput = '';
+        })
+    },
+    getPoster: function (obj) {
+      if (obj.poster_path) {
+        return `https://image.tmdb.org/t/p/w300_and_h450_bestv2${obj.poster_path}`;
+      } else {
+        return 'assets/img/not-img.jpg'
+      }
+    },
+    getTitle: function(obj) {
+      if (obj.title) {
+        return obj.title;
+      } else {
+        return obj.name;
+      }
+    },
+    getOriginalTitle: function(obj) {
+      if (obj.original_title) {
+        return obj.original_title;
+      } else {
+        return obj.original_name;
+      }
+    },
+    getVote: function(obj) {
+      return Math.ceil(obj / 2);
     }
-  }
+  },
 });
