fbc4e76a4b6858fcf72a591a1080efdc7cd4583d
Alessandro Brachetti <alebrack13@gmail.com>
2021-04-14 13:57:36 +0200

    Milestone 4 Completa

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,22 +5,29 @@ var app = new Vue({
   data: {
     uri: 'https://api.themoviedb.org/3',
     apiKey:'29e84b564b7c90c52ba93dc6436ea2c6',
-    lang: 'it',
+    lang: '',
     queryInput: '',
     movies: [],
+    errorMessage: false,
   },
   methods: {
+    // AXIOS CALL TO SEARCH A MOVIE AND TVSHOW
     search: function() {
+      this.movies = '';
       axios.get(`${this.uri}/search/movie?api_key=${this.apiKey}&language=${this.lang}&page=1&include_adult=false&query=${this.queryInput}`)
         .then((response) => {
+          console.log(response);
           this.movies = [...this.movies, ...response.data.results];
         })
       axios.get(`${this.uri}/search/tv?api_key=${this.apiKey}&language=${this.lang}&page=1&include_adult=false&query=${this.queryInput}`)
         .then((response) => {
+          console.log(response);
           this.movies = [...this.movies, ...response.data.results];
-          this.queryInput = '';
         })
+      this.queryInput = '';
+
     },
+    // GET MOVIE POSTERS OR IMG NOT AVAILABLE
     getPoster: function (obj) {
       if (obj.poster_path) {
         return `https://image.tmdb.org/t/p/w300_and_h450_bestv2${obj.poster_path}`;
@@ -28,6 +35,7 @@ var app = new Vue({
         return 'assets/img/not-img.jpg'
       }
     },
+    // GET TITLE OF MOVIES AND TVSHOWS
     getTitle: function(obj) {
       if (obj.title) {
         return obj.title;
@@ -35,6 +43,7 @@ var app = new Vue({
         return obj.name;
       }
     },
+    // GET ORIGINAL TITLE OF MOVIES AND TV SHOWS
     getOriginalTitle: function(obj) {
       if (obj.original_title) {
         return obj.original_title;
@@ -42,8 +51,9 @@ var app = new Vue({
         return obj.original_name;
       }
     },
+    // TRANSFORM VOTE IN 1/5
     getVote: function(obj) {
       return Math.ceil(obj / 2);
-    }
+    },
   },
 });
